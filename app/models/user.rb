8981b247e16c8e610b5d2a7db8e7f71f2b4e9776
Morrism1 <morrismontana0@gmail.com>
2021-06-02 01:24:17 +0200

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
