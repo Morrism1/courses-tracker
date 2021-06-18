89d7ecb64e42e845cc96842d46a4260d8ae44953
Morrism1 <morrismontana0@gmail.com>
2021-06-18 14:40:18 +0200

    Add routes regarding user registration and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: :index
-      post 'login', to: 'authentication#create'
+      post 'authenticate', to: 'authentication#create'
       post 'register', to: 'users#create'
     end
   end
