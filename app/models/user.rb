ba6fb5e97a9a03d58d9641b0d4ca46ab088f47da
Morrism1 <morrismontana0@gmail.com>
2021-06-18 16:33:45 +0200

    Add association to courses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :courses, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :password, presence: true, length: { minimum: 6 }
 end
