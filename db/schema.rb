d18943ab5aa377823fc77ca41f555264ea181100
Morrism1 <morrismontana0@gmail.com>
2021-06-18 16:38:41 +0200

    Add courses schema

--- db/schema.rb
+++ db/schema.rb
@@ -3,6 +3,7 @@
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # This file is the source Rails uses to define your schema when running `bin/rails
+
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
@@ -10,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_224628) do
+ActiveRecord::Schema.define(version: 2021_06_18_125628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "creator"
+    t.integer "hours"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_06_01_224628) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "courses", "users"
 end
